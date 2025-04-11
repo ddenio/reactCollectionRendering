09db1dfb2aa55ecbebcedf87a7b347fda0f7ba9f
Derin DeNio <derindenio@gmail.com>
2025-04-11 17:16:19 -0600

    Using map method to create lists

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,22 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const { notes } = props
-
+function App({notes}) {
+  //const { notes } = props
+  //console.log({notes})
   return (
     <div>
+      
       <h1>Notes</h1>
+      
       <ul>
+        {notes.map(note => <li>{note.content}</li>)}
+      </ul>
+      {/* <ul>
         <li>{notes[0].content}</li>
         <li>{notes[1].content}</li>
         <li>{notes[2].content}</li>
-      </ul>
+      </ul> */}
     </div>
   )
 }
